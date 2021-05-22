a519449c46cee22e3822c2f68f81df957c6ec7d4
ritikrishu <ritikrishu@gmail.com>
2021-05-22 17:35:27 +0530

    login page

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router';
+import { SdkModule } from '../sdk/sdk.module';
 
 
 
@@ -11,6 +12,7 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
+    SdkModule,
     RouterModule.forChild([
       {
         path: '',
